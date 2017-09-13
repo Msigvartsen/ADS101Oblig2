656ca7be4306742449a401bd53557aee54e4a7de
Magnus <magnus.sigvartsen@gmail.com>
2017-09-13 10:42:18 +0200

    minor changes til main

--- Oblig2/main.cpp
+++ Oblig2/main.cpp
@@ -3,37 +3,48 @@
 
 int main()
 {
-
+    std::cout << "----- char binary tree -----\n\n";
     TreeNode<char>* charNode = new TreeNode<char>('g');
     char charArray[9]{'e','f','c','d','a','i','h','k','j'};
+    std::cout << "Root of tree: " << charNode->getData() << "\n\n";
     std::cout << "Inserting chars to binary tree from array:\n";
     for(int i{0}; i < 9; i++)
     {
         std::cout << charArray[i] << " ";
         charNode->insert(charArray[i]);
     }
-    std::cout << "\nInOrder Traversal print:\n";
-
+    std::cout << "\n\nInOrder Traversal print:\n";
     charNode->inOrderTraversal();
-    std::cout << "\nPreOrder Traversal print:\n";
+
+    std::cout << "\n\nPreOrder Traversal print:\n";
     charNode->preOrderTraversal();
-    std::cout << "\npostOrder Traversal print:\n";
+
+    std::cout << "\n\npostOrder Traversal print:\n";
     charNode->postOrderTraversal();
-    std::cout << "\n";
+
+    std::cout << "\n\n";
 
 
-    TreeNode<int>* intNode = new TreeNode<int>(6);
-    intNode->insert(4);
-    intNode->insert(5);
-    intNode->insert(2);
-    intNode->insert(3);
-    intNode->insert(1);
-    intNode->insert(8);
-    intNode->insert(7);
-    intNode->insert(10);
-    intNode->insert(9);
 
-    intNode->inOrderTraversal();
+//    std::cout << "----- int binary tree -----\n\n";
+//    TreeNode<int>* intNode = new TreeNode<int>(6);
+//    int intArray[9]{4,5,2,3,1,8,7,10,9};
+//    std::cout << "Root of tree: " << charNode->getData() << "\n";
+//    std::cout << "\nInserting int to binary tree from array:\n";
+//    for(int i{0}; i < 9; i++)
+//    {
+//        std::cout << intArray[i] << " ";
+//        intNode->insert(intArray[i]);
+//    }
+
+//    std::cout << "\n\nInOrder Traversal print:\n";
+//    intNode->inOrderTraversal();
+
+//    std::cout << "\n\nPreOrder Traversal print:\n";
+//    intNode->preOrderTraversal();
+
+//    std::cout << "\n\npostOrder Traversal print:\n";
+//    intNode->postOrderTraversal();
 
 
     return 0;
